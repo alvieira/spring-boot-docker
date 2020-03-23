8bb905169a31104eb54889ded4151008fd0d82c9
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-03-22 22:25:48 -0300

    added page controller

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,10 +1,20 @@
 package guru.springframework.controllers;
 
-import guru.springframework.services.ProductService;
+import java.util.Date;
+import java.util.UUID;
+
+import javax.xml.bind.JAXBException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.model.events.PageViewEvent;
+import guru.springframework.pageview.PageViewService;
+import guru.springframework.services.ProductService;
 
 /**
  * Created by jt on 1/20/16.
@@ -12,17 +22,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+	private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+
     private ProductService productService;
+    private PageViewService pageViewService;
 
     @Autowired
-    public IndexController(ProductService productService) {
+    public IndexController(ProductService productService, PageViewService pageViewService) {
         this.productService = productService;
+        this.pageViewService = pageViewService;
     }
 
     @RequestMapping({"/", "index"})
-    public String getIndex(Model model){
+    public String getIndex(Model model) throws JAXBException{
 
         model.addAttribute("products", productService.listProducts());
+
+        //Send Page view event
+        PageViewEvent pageViewEvent = new PageViewEvent();
+        pageViewEvent.setPageUrl("springframework.guru/");
+        pageViewEvent.setPageViewDate(new Date());
+        pageViewEvent.setCorrelationId(UUID.randomUUID().toString());
+
+        log.info("Sending Message to page view service");
+        pageViewService.sendPageViewEvent(pageViewEvent);
 
         return "index";
     }
